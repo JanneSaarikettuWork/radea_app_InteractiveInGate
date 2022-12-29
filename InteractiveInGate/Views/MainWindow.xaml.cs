1927d6243d5184d7f88ba46c14d7797e0fc99e6a
Janne Saarikettu <janne.saarikettu.work@gmail.com>
2022-12-29 15:44:29 +0200

    Added MahApps library, modified code respectively. Added ViewModel.

--- InteractiveInGate/Views/MainWindow.xaml.cs
+++ InteractiveInGate/Views/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+using InteractiveInGate.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,17 +15,48 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
 
-namespace InteractiveInGate
+namespace InteractiveInGate.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            InteractiveInGateViewModel gate;
+
+            //try
+            //{
+            //    selfDiagnosticts = new SelfDiagnosticts();
+            //    gate = new RouterGateViewModel(selfDiagnosticts);
+            //}
+            //catch (Exception ex)
+            //{
+            //    NLog.LogManager.GetCurrentClassLogger().Error(ex);
+            //    this.ShowModalMessageExternal(Application.Current.FindResource("CriticalError") as string, ex.Message);
+            //    NLog.LogManager.Shutdown();
+            //    Application.Current.Shutdown();
+            //    return;
+            //}
+
+            //DataContext = gate;
+
+            //Navigator = new SingletonNavigator(RootFrame);
+            //Navigator.Navigate(SingletonNavigator.PageView.Start);
+            //Logo.Source = new BitmapImage(new Uri("../Assets/Comforta RGB.png", UriKind.RelativeOrAbsolute));
+
+            //gate.RegisterReader();
+            //selfDiagnosticts.Start(gate);
+        }
+
+
     }
 }
